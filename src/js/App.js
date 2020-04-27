b42607c9c2d153a6dcc17b65fe292475824e3566
Quentin Danneville <quentin@nextmap.io>
2020-04-27 12:32:07 +0200

    Initial commit with React and parcel bundler

--- src/js/App.js
+++ src/js/App.js
@@ -1,46 +1,13 @@
-import React, { useState } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
-
-import Home from './pages/home';
-import Login from './pages/login';
-import Layout from './components/layout';
-import PrivateRoute from './components/private-route';
-import { clearUser } from './utils/local-storage';
-
-//Style
-import "../less/style.less";
+import React from 'react';
 
 const App = () => {
-
-    const [user, setUser] = useState(null);
-
-    const signout = () => {
-        setUser(null);
-        clearUser();
-    }
-
     return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/login">
-                        <Layout>
-                            <Login setUser={setUser} />
-                        </Layout>
-                    </Route>
-                    <PrivateRoute user={user} path="/">
-                        <Layout signout={signout} user={user}>
-                            <Home />
-                        </Layout>
-                    </PrivateRoute>
-                </Switch>
-            </div>
-        </Router>
+        <h1>Hello world</h1>
     )
 }
 
-export default App;
+export default App
+
+// function App() {
+    //Code goes here
+// }
--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+
 const MOUNT_NODE = document.getElementById('app');
 
-ReactDOM.render(<App/>, MOUNT_NODE);
+ReactDOM.render(<App/>, MOUNT_NODE)
+
+{/* 
+    <div id="app">
+        <App>
+             <h1>Hello world</h1>
+        </App>
+    </div> 
+*/}
+
+
 
 
