f0c83d1d7287b89ec2f05157be6bb17b7909a50e
Quentin Danneville <quentin@nextmap.io>
2020-05-26 17:18:07 +0200

    Changed

--- src/js/store/auth.js
+++ src/js/store/auth.js
@@ -1,53 +1,53 @@
-// {
-//     auth {
-//         token:null,
-//         user:null,
-//         isLoading: false
+// // {
+// //     auth {
+// //         token:null,
+// //         user:null,
+// //         isLoading: false
+// //     }
+// // }
+
+// const token = (state = null, action) => {
+//     switch (action.type) {
+//         case "SET_AUTH_TOKEN":
+//             return action.payload;
+//         case "CLEAR_AUTH_TOKEN":
+//             return null;
+//         default:
+//             return state;
 //     }
 // }
 
-const token = (state = null, action) => {
-    switch (action.type) {
-        case "SET_AUTH_TOKEN":
-            return action.payload;
-        case "CLEAR_AUTH_TOKEN":
-            return null;
-        default:
-            return state;
-    }
-}
+// const user = (state = null, action) => {
+//     switch (action.type) {
+//         case "SET_AUTH_USER":
+//             return action.payload;
+//         case "CLEAR_AUTH_USER":
+//             return null;
+//         default:
+//             return state;
+//     }
+// }
 
-const user = (state = null, action) => {
-    switch (action.type) {
-        case "SET_AUTH_USER":
-            return action.payload;
-        case "CLEAR_AUTH_USER":
-            return null;
-        default:
-            return state;
-    }
-}
-
-const isLoading = (state = null, action) => {
-    switch (action.type) {
-        case "DOING_LOGIN":
-            return true;
-        case "FETCH_STORAGE":
-            return true;
-        case "LOGIN_FAILED":
-        case "SET_AUTH_USER":
-        case "SET_STORAGE":
-            return false;
-        default:
-            return state;
-    }
-};
+// const isLoading = (state = null, action) => {
+//     switch (action.type) {
+//         case "DOING_LOGIN":
+//             return true;
+//         case "FETCH_STORAGE":
+//             return true;
+//         case "LOGIN_FAILED":
+//         case "SET_AUTH_USER":
+//         case "SET_STORAGE":
+//             return false;
+//         default:
+//             return state;
+//     }
+// };
 
 
-const authReducer = combineReducers({
-    token,
-    user,
-    isLoading,
-});
+// const authReducer = combineReducers({
+//     token,
+//     user,
+//     isLoading,
+// });
 
-export default authReducer;
+// export default authReducer;
