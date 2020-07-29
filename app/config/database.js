69c7fae6a946f7fc0a8ec644a54b2ee31d01b4ed
Quentin Danneville <quentin@nextmap.io>
2020-07-29 16:04:40 +0200

    Strapi / React - redux initialization

---
+++ app/config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
+
+
+
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4c41aa93-353c-4fe4-a288-65181fdbc636'
+};
